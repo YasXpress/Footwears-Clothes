3a2cd9eb2f001020c94e50f9581ed64d526ef40b
YasXpress <yasxpress584@gmail.com>
2025-04-12 22:49:54 +0100

    Update Scripts.js

--- Free&Shipping/Scripts.js
+++ Free&Shipping/Scripts.js
@@ -304,7 +304,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-const database = getDatabase(app); // <-- You'll use this to access your database
+const db = getDatabase(app); // <-- You'll use this to access your database
   
   
   
